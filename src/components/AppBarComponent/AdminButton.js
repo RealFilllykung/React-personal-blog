a90aba2eb7a266d7cd6a245a2eb577da5e72128e
RealFilllykung <filllykunggm@gmail.com>
2021-09-11 11:38:56 +0700

    Added a draft of login and logout logic

--- src/components/AppBarComponent/AdminButton.js
+++ src/components/AppBarComponent/AdminButton.js
@@ -3,18 +3,29 @@ import { Button } from "react-bootstrap"
 import { Link } from "react-router-dom"
 
 function AdminButton(){
-    const [isLogin,setIsLogin] = useState(false)
-    const [buttonText,setButtonText] = useState('')
+    const [isLogin, setIsLogin] = useState(false)
+    const [buttonText, setButtonText] = useState('')
+    const [redirectLink, setRedirectLink] = useState('/login')
 
     useEffect(() =>{
         //Check whether the user is already login or not
+        setIsLogin(false)
         
-        //If the user already login, change the button text to Logout
-        isLogin ? setButtonText('Logout') : setButtonText('Login')
+        //If the user already login, change the button text to Logout and logout the user from the system
+        if (isLogin){
+            setButtonText('Logout')
+            //Logout the user
+            setRedirectLink('/')
+        }
+        else{
+            setButtonText('Login')
+        }
     },[isLogin])
 
+
+
     return (
-        <Link to="/login">
+        <Link to={redirectLink}>
             <Button variant="light">{buttonText}</Button>
         </Link>
     )
