aa76cc9ee3f7bbc5a87ad0c27e581efa2b1c82f9
RealFilllykung <filllykunggm@gmail.com>
2021-09-11 17:44:51 +0700

    Created the create post page

---
+++ src/components/CreatePostForm.js
@@ -0,0 +1,66 @@
+import { Row, Col, Form, Button } from 'react-bootstrap'
+
+export function Title(props){
+    const getter = props.title
+    const setter = props.setTitle
+
+    return(
+        <Row>
+            <Col>
+                <Form.Group>
+                    <Form.Label>Title</Form.Label>
+                    <Form.Control type="text" placeholder="Enter title" value={getter} onChange={(e) => setter(e.target.value)}></Form.Control>
+                </Form.Group>
+            </Col>
+        </Row>
+    )
+}
+
+export function ImageLink(props){
+    const getter = props.imageSrc
+    const setter = props.setImageSrc
+
+    return(
+        <Row>
+            <Col>
+                <Form.Group>
+                    <Form.Label>Thumbnail Image Link</Form.Label>
+                    <Form.Control type="text" placeholder="Enter title" value={getter} onChange={(e) => setter(e.target.value)}></Form.Control>
+                </Form.Group>
+            </Col>
+        </Row>
+    )
+}
+
+export function Content(props){
+    const getter = props.content
+    const setter = props.setContent
+
+    return(
+        <Row>
+            <Col>
+                <Form.Group>
+                    <Form.Label>Content</Form.Label>
+                    <Form.Control as="textarea" rows={10} value={getter} onChange={(e) => setter(e.target.value)}></Form.Control>
+                </Form.Group>
+            </Col>
+        </Row>
+    )
+}
+
+export function SubmitPost(props){
+    const imageSrc = props.imageSrc
+    const title = props.title
+    const content = props.content
+
+    function handleClick(){
+        //Send all the information to the Firebase database
+        
+        //Redirect to the homepage
+    }
+
+
+    return(
+        <Button variant="primary" onClick={() => handleClick()}>Create post</Button>
+    )
+}
--- src/views/CreatePost.js
+++ src/views/CreatePost.js
@@ -1,9 +1,22 @@
+import { Title, ImageLink, Content, SubmitPost } from '../components/CreatePostForm'
+
+import { useState } from "react"
+import { Container, Row, Col } from 'react-bootstrap'
+
 function CreatePost(){
+
+    //All of this information is for thumbnail
+    const [imageSrc, setImageSrc] = useState('')
+    const [titleText, setTitleText] = useState('')
+    const [contentText, setContentText] = useState('')
     
     return(
-        <div>
-            Create post page
-        </div>
+        <Container className="mt-5">
+            <Title title={titleText} setTitle={setTitleText}></Title>
+            <ImageLink imageSrc={imageSrc} setImageSrc={setImageSrc}></ImageLink>
+            <Content content={contentText} setContent={setContentText}></Content>
+            <SubmitPost title={titleText} imageSrc={imageSrc} content={contentText}></SubmitPost>
+        </Container>
     )
 }
 
